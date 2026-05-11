671d794f181ccde39635bb1126db2176635d528b
mrhasantanvir <smmhasantanvir@gmail.com>
2026-05-11 15:24:47 +1000

    Install GTM and finalize tracking codes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5WHQSG6C"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WNP9GQGF"
             height="0" 
             width="0" 
             style={{ display: "none", visibility: "hidden" }}
@@ -64,7 +64,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5WHQSG6C');
+            })(window,document,'script','dataLayer','GTM-WNP9GQGF');
           `}
         </Script>
         <Script
